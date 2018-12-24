0f9816eb670511ad4757f210dfbbc9421a8051bd
Manjula Jayawardana <manjulajayawardana@gmail.com>
2018-12-24 08:46:33 +0530

    register category api with eureka service discovery

--- category-api/src/main/java/com/slmanju/ecommerce/categoryapi/CategoryApiApplication.java
+++ category-api/src/main/java/com/slmanju/ecommerce/categoryapi/CategoryApiApplication.java
@@ -2,8 +2,10 @@ package com.slmanju.ecommerce.categoryapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CategoryApiApplication {
 
 	public static void main(String[] args) {
