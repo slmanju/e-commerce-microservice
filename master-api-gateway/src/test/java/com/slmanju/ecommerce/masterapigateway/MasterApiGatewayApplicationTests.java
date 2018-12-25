311617975355b76005ca0ab8946b3668fa1e2b89
Manjula Jayawardana <manjulajayawardana@gmail.com>
2018-12-25 19:31:15 +0530

    add zuul api gateway

---
+++ master-api-gateway/src/test/java/com/slmanju/ecommerce/masterapigateway/MasterApiGatewayApplicationTests.java
@@ -0,0 +1,17 @@
+package com.slmanju.ecommerce.masterapigateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MasterApiGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
