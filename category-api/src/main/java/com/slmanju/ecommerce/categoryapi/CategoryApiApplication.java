1ed6cf9b1d94f2cf5b275c5232bb03008abd2871
Manjula Jayawardana <manjulajayawardana@gmail.com>
2018-12-26 07:42:53 +0530

    fix api gateway service discovery

--- category-api/src/main/java/com/slmanju/ecommerce/categoryapi/CategoryApiApplication.java
+++ category-api/src/main/java/com/slmanju/ecommerce/categoryapi/CategoryApiApplication.java
@@ -2,10 +2,10 @@ package com.slmanju.ecommerce.categoryapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class CategoryApiApplication {
 
 	public static void main(String[] args) {
